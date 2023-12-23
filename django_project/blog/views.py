f1652dcb28bf539a800c2747fa1b32be03cdca1f
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2023-12-23 22:20:53 +0530

    Add files via upload

---
+++ django_project/blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author':'rafay',
+        'title':'Blog post1',
+        'content':'first page content',
+        'date': 'august,27,2023'
+
+    },
+    {
+         'author':'sami',
+        'title':'Blog post2',
+        'content':'second page content',
+        'date': 'august,28,2023'
+
+    }
+]
+
+def home(request):
+    context={
+        'posts': posts
+    }
+    return render(request,'blog/home.html',context)
+
+
+
+def about(request):
+    return render(request,'blog/about.html')
+
