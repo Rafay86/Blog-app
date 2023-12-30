888bd8fad391f4504e5ede12044fb71082a44540
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2023-12-30 21:07:03 +0530

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap4',
     'blog.apps.BlogConfig',
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',,
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
