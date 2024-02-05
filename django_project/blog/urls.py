b68b311b4b07a4488a0abcd63d4ee0443a35cc2e
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-05 15:50:27 +0530

    Update urls.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
     path('about/', views.about,name='blog-about')
 ]
