c8172c0aa6129cdb5c03ad2f7f35d05b58c34eb3
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-05 15:50:54 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
 from .models import post
 
 
@@ -8,8 +9,16 @@ def home(request):
     }
     return render(request,'blog/home.html',context)
 
+class PostListView(ListView):
+    model= post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-
+class PostDetailView(DetailView):
+    model= post
+    
 def about(request):
-    return render(request,'blog/about.html',{'title':'about'})
+    return render(request,'blog/about.html',{'title':'About'})
+
 
