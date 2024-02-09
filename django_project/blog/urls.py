3b36b9e57fa737064816b8e94b8c3c025d507594
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-09 21:08:12 +0530

    Update urls.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView ,
+    PostDetailView,
+    PostCreateView)
+from .import views
 
 urlpatterns = [
-    path('', views.home,name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
-    path('about/', views.about,name='blog-about')
+    path('', PostListView.as_view(),name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view() ,name='post-detail'),
+    path('post/new/', PostCreateView.as_view() ,name='post-create'),
+    path('about/', views.about,name='blog-about'),
 ]
