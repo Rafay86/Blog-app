5f15460fabd726abfac9e2a53db3b8ff2d0e8f64
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-09 21:07:49 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
-from .models import post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView)
+from .models import Post
 
 
 def home(request):
     context={
-        'posts': post.objects.all()
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
 class PostListView(ListView):
-    model= post
+    model= Post                              #<app>/<model>_<viewtype>.html
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
-    model= post
+    model= Post
+    
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model= Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
+
 
 
