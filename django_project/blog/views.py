d6b0f14e786cca841d69771b5ddbeb9b7f78a506
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-10 19:53:19 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,6 +6,16 @@ from django.views.generic import (
     CreateView)
 from .models import Post
 
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView)
+from .models import Post
+
 
 def home(request):
     context={
@@ -31,7 +41,34 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model= Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)    
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model= Post    
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+    
+
+
 def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
     return render(request,'blog/about.html',{'title':'About'})
 
 
