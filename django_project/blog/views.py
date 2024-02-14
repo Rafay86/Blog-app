39b956278683921aef024094b1bc89c680785d52
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-14 23:14:49 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView)
-from .models import Post
-
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -28,8 +21,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by=2
+    paginate_by=5
 
+class UserPostListView(ListView):
+    model= Post                              #<app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by=5
+
+    def get_query_set(self):
+        user= get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model= Post
@@ -70,7 +73,8 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
-    return render(request,'blog/about.html',{'title':'About'})
+
+
 
 
 
