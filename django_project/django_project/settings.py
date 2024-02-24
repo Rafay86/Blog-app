0600b25e206d9a24aa4a663301143772b0af1ec3
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-24 23:10:27 +0530

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -133,3 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 'CRISPY_TEMPLATE_PACK'='bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
