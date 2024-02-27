bf9ce2624aaf7f43dbcf39f3a7bdd046095d03ad
mohd rafay <95489841+Rafay86@users.noreply.github.com>
2024-02-27 23:13:19 +0530

    Update urls.py

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -35,13 +35,19 @@ urlpatterns = [
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'), 
          name = 'password_reset_done'),     
-    path('password-reset-confirm/<uidb64><token>/',
+    path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), 
-         name = 'password_reset_confirm'),          
+         name = 'password_reset_confirm'),      \
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'), 
+         name = 'password_reset_complete'),
+
     path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 
